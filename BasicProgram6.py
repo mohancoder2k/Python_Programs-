6e1ba62e84ed0c7aecbfc206e23a7470585dd055
mohancoder2k <mohanbalu2k5@gmail.com>
2024-02-26 12:58:49 +0530

    updated

--- BasicProgram6.py
+++ BasicProgram6.py
@@ -3,6 +3,7 @@ num1=int(input("Enter first Number : "))
 num2=int(input("Enter second number : "))
 avg = num1 + num2/2
 print("Average of two Numbers : ", avg)
-print("Deviation : ")
+print("Deviation  of first number : ", num1-avg)
+print("Deviation  of second number : ", num2-avg)
 
 #python program to find total amount of money in piggy bank, the coin should be rs.5, rs.10, rs.2 and rs.1 coins. 
